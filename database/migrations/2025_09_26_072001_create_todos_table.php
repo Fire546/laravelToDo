e89273b194b906b392d67c547b4e8c981ad1b3b7
Nero <jozhenya@gmail.com>
2025-09-26 15:41:08 +0500

    change to todo foreing id to foreign uuid

--- database/migrations/2025_09_26_072001_create_todos_table.php
+++ database/migrations/2025_09_26_072001_create_todos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->text('text');
-            $table->foreignId('user_id');
+            $table->foreignUuid('user_id');
             $table->timestamps();
         });
     }
